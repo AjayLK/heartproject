83cbd55ea0167e0a260494f1413432a9032b4d4c
Ajay <ajaykshirsagar500@gmail.com>
2022-11-02 23:52:19 +0530

    Add all file

---
+++ config.py
@@ -0,0 +1,11 @@
+
+import os
+
+
+CSV_FILE_PATH = "enter_path"
+
+PORT_NUMBER = 2345
+
+MODEL_FILE_PATH = os.path.join("heart_model" , "heart_model.pkl")
+
+JSON_FILE_PATH = os.path.join("heart_model" , "json_heart_data.json")
